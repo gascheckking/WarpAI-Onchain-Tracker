c2a0c8c63c5e410232066c2681b97bcbfc165d85
gascheckking <warpaiactivity1@gmail.com>
2025-05-22 20:33:13 +0200

    Update tracker.js

--- pages/tracker.js
+++ pages/tracker.js
@@ -1,1 +1,10 @@
+import OnchainActivity from '../components/OnchainActivity';
 
+export default function Tracker() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-4">
+      <h1 className="text-2xl font-bold mb-6">🔍 Onchain Tracker</h1>
+      <OnchainActivity />
+    </div>
+  );
+}
