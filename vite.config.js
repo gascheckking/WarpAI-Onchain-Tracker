0f21efbf6d3ec7be05495b493c348aa70a15f3b0
gascheckking <warpaiactivity1@gmail.com>
2025-05-22 21:40:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
+  server: {
+    port: 3000,
+    open: true
   }
-});
+})
