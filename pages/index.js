e46214ba05c04d85270a73c8836c2e34ca5f087e
gascheckking <warpaiactivity1@gmail.com>
2025-05-22 20:29:42 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
+import GasMeter from '../components/GasMeter';
 
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <GasMeter />
+      {/* Resten av din UI här */}
+    </main>
+  );
+}
