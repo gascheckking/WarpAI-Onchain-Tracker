6c339345fdb034582f8abb4a8fbe9458e85d00f7
gascheckking <warpaiactivity1@gmail.com>
2025-05-22 20:33:22 +0200

    Update rewards.js

--- pages/rewards.js
+++ pages/rewards.js
@@ -1,1 +1,12 @@
+import Leaderboard from '../components/Leaderboard';
+import RewardTasks from '../components/RewardTasks';
 
+export default function Rewards() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-4">
+      <h1 className="text-2xl font-bold mb-6">🎁 Earn WARP Tokens</h1>
+      <RewardTasks />
+      <Leaderboard />
+    </div>
+  );
+}
