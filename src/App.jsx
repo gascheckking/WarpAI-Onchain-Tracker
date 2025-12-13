311644f7c1461d1a4599f233f1488b58032e4872
gascheckking <warpaiactivity1@gmail.com>
2025-12-13 23:51:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/index';
-import Tracker from './pages/tracker';
-import Rewards from './pages/rewards';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Tracker from "./pages/Tracker";
+import Rewards from "./pages/Rewards";
 
 export default function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/tracker" element={<Tracker />}
+        <Route path="/tracker" element={<Tracker />} />
+        <Route path="/rewards" element={<Rewards />} />
+      </Routes>
+    </div>
+  );
+}
